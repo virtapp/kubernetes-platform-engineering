00611c8842480f8eaa6fba8db73a9afd1510fc79
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-08-02 22:15:55 +0300

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     null = {
@@ -8,15 +9,12 @@ terraform {
 }
 
 provider "null" {}
-
 resource "null_resource" "install_k3s" {
   provisioner "local-exec" {
     command = "export K3S_EXEC_FLAGS='--disable=traefik --disable=local-storage' && bash ./modules/k3s/install_k3s.sh ${var.node_role} ${var.k3s_version}"
   }
-
   triggers = {
     always_run = "${timestamp()}"
-
   }
 }
 
@@ -27,10 +25,12 @@ resource "null_resource" "k3s_config" {
   depends_on = [null_resource.install_k3s]
 }
 
-
 resource "null_resource" "k3s_status" {
   provisioner "local-exec" {
     command = "sleep 5 && systemctl status k3s | grep -v k3s 2>/dev/null"
   }
   depends_on = [null_resource.k3s_config]
 }
+
+
+
