8bb1071a54dfe0df04dff2af7495339e92212045
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-09 20:54:23 +0300

    update

--- resources.tf
+++ resources.tf
@@ -21,51 +21,7 @@ module "argo" {
 }
 
 # 4️⃣ Ingress for the app
-resource "kubernetes_ingress_v1" "argo-ingress" {
-  depends_on = [module.argo]
-  metadata {
-    name      = "ingress-route-argo"
-    namespace = "argocd"
-    labels = {
-      name = "argocd"
-    }
-    annotations = {
-      "kubernetes.io/ingress.class" = "nginx"
-      "nginx.ingress.kubernetes.io/backend-protocol" = "HTTPS"
-      "nginx.ingress.kubernetes.io/affinity" = "cookie"
-      "nginx.ingress.kubernetes.io/session-cookie-name" = "route-argo"
-      "nginx.ingress.kubernetes.io/session-cookie-expires" = "172800"
-      "nginx.ingress.kubernetes.io/session-cookie-max-age" = "172800"
-      "nginx.ingress.kubernetes.io/proxy-connect-timeout" = "300s"
-      "nginx.ingress.kubernetes.io/proxy-send-timeout" = "300s"
-      "nginx.ingress.kubernetes.io/proxy-read-timeout" = "300s"
-      "nginx.ingress.kubernetes.io/send-timeout" = "300"
-      "nginx.ingress.kubernetes.io/client-body-buffer-size" = "5m"
-      "nginx.ingress.kubernetes.io/proxy-body-size" = "5m"
-    }
-  }
 
-  spec {
-    rule {
-      host = "argo.local.com"
-      http {
-        path {
-          backend {
-            service{
-              name = "argocd-server"
-              port {
-               number = 80
-               }
-          }
-        }
-      }
-    }
-  }
-  tls {
-      secret_name = "argo-tls"
-    }
- }
-}
 
 
 
